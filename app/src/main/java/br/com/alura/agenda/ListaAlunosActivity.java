0cff5ac970b6471563662b6e4939ce1a7f6ee2cb
ruicosenza <ruicosenza@gmail.com>
2017-11-27 13:40:30 -0200

    Primeiro commit

---
+++ app/src/main/java/br/com/alura/agenda/ListaAlunosActivity.java
@@ -0,0 +1,21 @@
+package br.com.alura.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Tamires", "Rui", "Danny"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+    }
+}
